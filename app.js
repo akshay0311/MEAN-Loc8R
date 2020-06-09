23e097413cb92008be48cae93ef0813e6cbf0ca7
akki <akshaycoding123@gmail.com>
2020-06-09 13:47:30 +0530

    Version-1.0.0 commit

--- app.js
+++ app.js
@@ -11,27 +11,23 @@ var mongoose = require("mongoose")
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
-// for api
-var indexAPIRouter = require('./app_api/routes/index');
-
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/app_server/views'));
  
 app.engine('.hbs', hbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_server/public')));
 
 app.use('/', indexRouter);
-app.use('/api',indexAPIRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
